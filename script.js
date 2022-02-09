8967b23af2c7da0ecb8fc73c380763cd1db2549b
garysu92 <garysu929292@gmail.com>
2022-02-08 21:35:37 -0500

    made the operations be clicked and typed into the boxes

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let w = 10;
 let col = 1;
 let row = 1;
 
+const box = document.getElementById("box");
+
 window.onload = function (){
     start();
 }
 
 function start () {
-    const box = document.getElementById("box");
     for (let i = 1; i <= h; i++) {
         for (let j = 1; j <= w; j++) {
             let tile = document.createElement("span");
@@ -34,3 +35,33 @@ function start () {
         }
     })
 }
+
+function onClick() {
+    if (col <= w && row <= h) {
+        let block = document.getElementById(row.toString() + "-" + col.toString());
+        if (this.id == "plus") {
+            console.log("gdsaf");
+            block.innerText = '+';
+        } else if (this.id == 'minus') {
+            block.innerText = "−";
+        } else if (this.id == 'times') {
+            block.innerText = "×";
+        } else if (this.id == 'divide') {
+            block.innerText = "÷";
+        } else if (this.id == 'equal') {
+            block.innerText = "=";
+        }
+        if (col == w) {
+            row++;
+            col = 1;
+        } else {
+            col++;
+        }
+    }
+}
+
+document.getElementById('plus').onclick = onClick;
+document.getElementById('minus').onclick = onClick;
+document.getElementById('times').onclick = onClick;
+document.getElementById('divide').onclick = onClick;
+document.getElementById('equal').onclick = onClick;
