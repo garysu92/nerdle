ba78154e357c56c1a34c4878dc9a65dbf213e1b6
garysu92 <garysu929292@gmail.com>
2022-02-09 22:29:11 -0500

    generate equation on refresh with 1 operation

--- script.js
+++ script.js
@@ -4,13 +4,78 @@ let w = 8;
 let col = 1;
 let row = 1;
 
+let num1 = 0;
+let num2 = 0;
+let op = 0;
+let ans = 0;
+
+const box = document.getElementById("box");
+
+let nextRow = true;
+let valid = false;
+
+let ansBlock = [];
+
+let iter = 0;
+
 window.onload = function () {
   start();
 };
 
 function start() {
-  
-  
+  op = Math.floor(Math.random() * 4 + 1);
+  while (!valid) {
+    num1 = Math.floor(Math.random() * 999 + 1);
+    num2 = Math.floor(Math.random() * 999 + 1);
+    let remaining = 8 - 2 - num1.toString().length - num2.toString().length;
+    let ans;
+    if (op == 1) {
+      ans = num1 + num2;
+    } else if (op == 2) {
+      ans = num1 - num2;
+    } else if (op == 3) {
+      ans = num1 * num2;
+    } else {
+      ans = num1 / num2;
+    }
+    if (op == 4 && ans * num2 != num1) {
+      continue;
+    }
+
+    if (ans.toString().length != remaining) {
+      continue;
+    } else {
+        valid = !valid;
+        let arrNum1 = num1.toString().split("");
+        let arrNum2 = num2.toString().split("");
+        let arrNum3 = ans.toString().split("");
+        for (let i = 0; i < arrNum1.length; i++) {
+            ansBlock[iter] = arrNum1[i];
+           iter++;
+        }
+        if (op == 1) {
+            ansBlock[iter] = "+";
+        } else if (op == 2) {
+            ansBlock[iter] = "−";
+        } else if (op == 3) {
+            ansBlock[iter] = "×";
+        } else {
+            ansBlock[iter] = "÷";
+        }
+        iter++;
+        for (let i = 0; i < arrNum2.length; i++) {
+            ansBlock[iter] = arrNum2[i];
+            iter++;
+        }
+        ansBlock[iter] = "=";
+        iter++;
+        for (let i = 0; i < arrNum3.length; i++) {
+            ansBlock[iter] = arrNum3[i];
+            iter++;
+        }
+        console.log(ansBlock);
+    }
+  }
   for (let i = 1; i <= h; i++) {
     for (let j = 1; j <= w; j++) {
       let tile = document.createElement("span");
